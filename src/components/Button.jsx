5f5413b41020f861460dfb5149199b85ce6684d1
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-03-21 23:49:12 +0300

    complete carddeal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none mt-5`}>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins rounded-[10px] font-medium text-[18px] text-primary outline-none mt-5`}>
       Get Started
     </button>
   )
