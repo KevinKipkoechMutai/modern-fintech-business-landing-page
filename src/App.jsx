652c68f51f178aecfcf4c3cce02ba95908b27513
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-03-22 12:28:37 +0300

    complete testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
+    <div className={`bg-primary w-full overflow-hidden`}>
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
             <Navbar/>
